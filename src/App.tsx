3bb1e7d83f69b5114993d8ec8dd9092256cd0135
Grecia Garcia <garciav.grecia@gmail.com>
2022-03-06 18:01:57 -0500

    add provider and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
     return(
-        <div>
-            Hello React
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Provider store={store}>Hello React</Provider>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
