51834f142ddbace056e6923826892d8f41ced0b3
Grecia Garcia <garciav.grecia@gmail.com>
2022-03-07 00:53:13 -0500

    Make progress of ToggleCart component

--- src/components/ToggleCart/ToggleCart.tsx
+++ src/components/ToggleCart/ToggleCart.tsx
@@ -1,18 +1,5 @@
 import React from 'react';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Divider } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, Divider, Drawer, List, Typography } from '@mui/material';
 
 type Anchor = 'right';
 
@@ -51,7 +38,7 @@ export default function ToggleCart() {
 						"name": "Real Madrid",
 						"price": 1.23
 					},].map((item) => (
-          <Card sx={{ minWidth: 275, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+          <Card key={item.id} sx={{ minWidth: 275, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
             <CardContent key={item.id}> 
               <Typography variant="h5" component="div">
                 {item.name}
