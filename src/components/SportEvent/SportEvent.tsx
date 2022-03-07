ad8271cbda7dedb66f593928d4562806d0845412
Grecia Garcia <garciav.grecia@gmail.com>
2022-03-07 00:51:20 -0500

    Complete SportEvent component

--- src/components/SportEvent/SportEvent.tsx
+++ src/components/SportEvent/SportEvent.tsx
@@ -1,27 +1,49 @@
 import React from "react";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
+import {Box, Divider, List, Typography} from '@mui/material';
 import SportEventMarket from "../SportEventMarket/SportEventMarket";
 
 export default function SportEvent() {
 
+    const listEvents = [{
+		"id": "EVT_1",
+		"name": "Real Madrid vs Barcelona",
+		"markets": []
+	},
+	{
+		"id": "EVT_2",
+		"name": "Atletico Madrid vs Malaga",
+		"markets": []
+	},
+	{
+		"id": "EVT_3",
+		"name": "Empty Event that shouldn't render",
+		"markets": []
+	}]
+
     return(
-        <Box 
-            style={{ width: '100%' }}
-            sx={{border: '2px solid grey' }}>               
-            <Typography component="div">
-                <Box sx={{ 
-                        fontWeight: 'bold', 
-                        m: 1, 
-                        textAlign: 'center',
-                        textTransform: 'uppercase'
-                        }}>
-                         Real Madrid vs Barcelona
-                    </Box>
-            </Typography>
-            <Divider />
-            <SportEventMarket />           
-        </Box>        
+        <div style={{ width: '100%' }}>
+            <List sx={{alignItems: 'center' }}>
+                {listEvents.map((event) => (
+                    <Box 
+                    style={{ width: '100%' }}
+                    sx={{border: '2px solid grey', mb: 2 }}>               
+                    <Typography component="div">
+                        <Box sx={{ 
+                                fontWeight: 'bold', 
+                                m: 1, 
+                                textAlign: 'center',
+                                textTransform: 'uppercase'
+                                }}>
+                                {event.name}
+                            </Box>
+                    </Typography>
+                    <Divider />
+                    <SportEventMarket />           
+                </Box>  
+                ))}
+            </List>
+        </div>
+
+              
     )    
 }
